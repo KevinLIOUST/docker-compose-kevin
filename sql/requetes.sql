6f31e339a6a02203323293f0a860a00ad55b0618
KevinLIOUST <kevin.lioust@outlook.fr>
2025-07-21 17:03:28 +0200

    Monde 7 Avancement  ! :)

--- sql/requetes.sql
+++ sql/requetes.sql
@@ -171,9 +171,11 @@ SELECT MAX(prix) FROM `lpecom_livres`;
 SELECT titre, prix FROM `lpecom_livres` ORDER BY prix DESC LIMIT 1;
 
 -- Monde 5-5
+-- Afficher les livres de la table lpecom_livres qui ont un prix compris entre 20 et 22.
 SELECT titre, prix FROM `lpecom_livres` WHERE prix >= 20 AND prix <= 22 ORDER BY titre ASC;
 
 -- Monde 5-6
+-- Afficher les livres de la table lpecom_livres à l'exception de celui portant la valeur pour la colonne isbn_10 : 2092589547.
 SELECT titre FROM `lpecom_livres` WHERE isbn_10 != 2092589547 ORDER BY titre ASC;
 
 -- Monde 6
@@ -188,10 +190,12 @@ SELECT COUNT(id_etudiant) AS nbr_doublon, id_etudiant, matiere FROM `lpecom_exam
 -- puis on la connecte avec la table lpecom_examens pour afficher les informations générales.
 -- Puis avec l'aide de HAVING, on regarde si le nombre de fois où apparaît le même id_etudiant est vraiment plus grand que 1 et on range les lignes avec les id_etudiant du plus petit au plus grand.
 SELECT id_etudiant FROM `lpecom_etudiants` NATURAL JOIN `lpecom_examens` GROUP BY id_etudiant HAVING COUNT(id_etudiant) > 1 ORDER BY id_etudiant ASC;
+SELECT id_etudiant FROM lpecom_examens;
 
 -- Monde 6-2
 -- Compter le nombre d'étudiants qui ont participés à au moins un examen.
 SELECT COUNT(id_etudiant), id_etudiant FROM `lpecom_etudiants` NATURAL JOIN `lpecom_examens` GROUP BY id_etudiant HAVING COUNT(id_etudiant) > 1 ORDER BY id_etudiant ASC;
+SELECT COUNT(DISTINCT id_etudiant) FROM lpecom_examens;
 
 -- Monde 6-3
 -- Calculer la moyenne de l'examen portant l'id 45.
@@ -212,6 +216,7 @@ SELECT DISTINCT id_etudiant FROM `lpecom_etudiants` NATURAL JOIN `lpecom_examens
 -- Monde 6-6
 -- Afficher tous les enregistrement de la table lpecom_examens avec en plus, si c'est possible, le prénom et le nom de l'étudiant.
 SELECT * FROM `lpecom_examens` NATURAL JOIN `lpecom_etudiants`;
+SELECT * FROM lpecom_examens as exam LEFT JOIN lpecom_etudiants as etu ON exam.id_etudiant = etu.id_etudiant;
 
 -- Monde 6-7
 -- Afficher les enregistrements de la table lpecom_examens avec le prénom et le nom de l'étudiant, uniquement quand les étudiants sont présents dans la table lpecom_etudiants.
@@ -229,30 +234,63 @@ SELECT nom, prenom, matiere, note FROM `lpecom_etudiants` NATURAL JOIN `lpecom_e
 -- Monde 7
 
 -- Monde 7-1
+-- Listez l’ensemble des jeux enregistré dans la table « games ».
+SELECT * FROM games;
 
 -- Monde 7-2
+-- Listez l’ensemble de jeux sans les doublons.
+SELECT DISTINCT g_name FROM games ORDER BY g_name ASC;
 
 -- Monde 7-3
+-- Affichez le nom, le mode de jeu, la date de sortie et le pegi de tous les jeux triés par ordre alphabétiques.
+SELECT g_name, g_mode, g_published_at, g_pegi FROM games ORDER BY g_name ASC;
+SELECT DISTINCT g_name, g_mode, g_published_at, g_pegi FROM games ORDER BY g_name ASC;
 
 -- Monde 7-4
+-- Affichez le nom, le mode de jeu, la date de sortie et le pegi des 10 jeux les plus récents.
+SELECT g_name, g_mode, g_published_at, g_pegi FROM games ORDER BY g_published_at DESC LIMIT 10;
 
 -- Monde 7-5
+-- Affichez le nom et le mode de jeu des jeux qui se jouent uniquement dans un seul mode.
+SELECT g_name, g_mode FROM games WHERE g_mode = "Solo" || g_mode = "Multijoueur";
 
 -- Monde 7-6
+-- Affichez le nom et la date de sortie des jeux sortie entre 2015 et 2020 triés par année
+SELECT DATE_FORMAT(g_published_at, "%Y"), g_name FROM games WHERE g_published_at BETWEEN '2015-01-01' AND '2020-12-31' ORDER BY g_published_at ASC;
+SELECT g_published_at, g_name FROM games WHERE g_published_at BETWEEN '2015-01-01' AND '2020-12-31' ORDER BY g_published_at ASC;
 
 -- Monde 7-7
+-- Affichez le nom et le mode de jeu des jeux qui peuvent se jouer en mode solo
+SELECT * FROM games;
+SELECT g_name, g_mode FROM games WHERE g_mode = 'Solo' || g_mode = 'Solo / Multijoueur' || g_mode = 'Solo / Coopératif';
+SELECT g_name, g_mode FROM games WHERE g_mode != 'Multijoueur';
 
 -- Monde 7-8
+-- Affichez les informations des différents jeux "witcher" disponibles.
+SELECT * FROM games;
+SELECT * FROM games WHERE g_name LIKE "The W%";
 
 -- Monde 7-9
+-- Affichez les informations de tous les jeux sauf les jeux "Halo".
+SELECT * FROM games WHERE g_name NOT LIKE "Halo%";
 
 -- Monde 7-10
+-- Lister les jeux sortis en 2012, 2016 et 2020.
+SELECT * FROM games
+WHERE g_published_at LIKE '2012%' OR g_published_at LIKE '2016%' OR g_published_at LIKE '2020%'
+ORDER BY g_published_at ASC;
 
 -- Monde 7-11
+-- Affichez le nom de jeu et le studio de tous les jeux. Utiliser une jointure naturelle.
+SELECT g_name, s_name FROM games NATURAL JOIN studios;
 
 -- Monde 7-12
+-- Affichez le nom de jeu, le studio, la nationalité de la société des jeux disponibles. Utiliser une jointure avec join
+SELECT g_name, s_name, s_nationality FROM games NATURAL JOIN studios;
 
 -- Monde 7-13
+-- Affichez le nom et le mode de jeu des jeux console grand public triés par pegi croissant
+SELECT g_name, g_mode, g_pegi, p_name FROM games NATURAL JOIN platforms ORDER BY g_pegi ASC;
 
 -- Monde 7-14
 
