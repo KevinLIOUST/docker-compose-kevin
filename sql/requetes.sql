be00a526d31bd751409abc91a14b5e2cd3a1b49f
KevinLIOUST <kevin.lioust@outlook.fr>
2025-07-21 13:01:37 +0200

    Monde 6 terminé  ! :)

--- sql/requetes.sql
+++ sql/requetes.sql
@@ -1,29 +1,38 @@
 -- Monde 1
 
 -- Monde 1-1
+-- Créer une base de données “languages”.
 CREATE DATABASE languages;
 
 -- Monde 1-2
+-- Créer une base de données “webdevelopment“avec l’encodage “UTF-8”.
 CREATE DATABASE webdevelopment CHARACTER SET utf8;
 
 -- Monde 1-3
+-- Créer une base de données “frameworks”,si elle n’existe pas, avec l’encodage “UTF-8”.
 CREATE DATABASE IF NOT EXISTS frameworks CHARACTER SET utf8;
 
 -- Monde 1-4
+-- Créer une base de données “languages”,si elle n’existe pas,avec l’encodage “UTF-8”.
 CREATE DATABASE IF NOT EXISTS languages CHARACTER SET utf8;
 
 -- Monde 1-5
+-- Supprimer la base de données “languages”.
 DROP DATABASE languages;
 
 -- Monde 1-6
+-- Supprimer la base de données,si elle existe, “frameworks”.
 DROP DATABASE IF EXISTS frameworks;
 
 -- Monde 1-7
+-- Supprimer la base de données,si elle existe, “languages”.
 DROP DATABASE IF EXISTS languages;
 
 -- Monde 1-8
 
 -- Selon moi, c'est normal de vérifier avant la création ou la suppression de la base de données si elle existe déjà ou pas, pour éviter de la récréer.
+-- Selon vous, quel est l’intérêt de vérifier si une base de données existe avant la création / suppression ?
+-- Avant de terminer s’assurer que la base de données “webdevelopment”est bien présente dans votre SGBDR.
 
 DROP DATABASE IF EXISTS webdevelopment;
 CREATE DATABASE IF NOT EXISTS webdevelopment;
@@ -31,6 +40,7 @@ CREATE DATABASE IF NOT EXISTS webdevelopment;
 -- Monde 2
 
 -- Monde 2-1
+
 CREATE TABLE language (
 id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
 langue VARCHAR(50)
@@ -173,18 +183,85 @@ SELECT titre FROM `lpecom_livres` WHERE isbn_10 != 2092589547 ORDER BY titre ASC
 SELECT TOP 3 id FROM `lpecom_examens`;
 SELECT COUNT(id_etudiant) AS nbr_doublon, id_etudiant, matiere FROM `lpecom_examens` GROUP BY id_etudiant, matiere HAVING COUNT(id_etudiant) > 1 ORDER BY matiere;
 
+-- VRAI REQUETE :
+-- On sélectionne l'id des étudiants, puis le nombre de fois où le même id_etudiant apparaît dans la table de la table lpecom_etudiants,
+-- puis on la connecte avec la table lpecom_examens pour afficher les informations générales.
+-- Puis avec l'aide de HAVING, on regarde si le nombre de fois où apparaît le même id_etudiant est vraiment plus grand que 1 et on range les lignes avec les id_etudiant du plus petit au plus grand.
+SELECT id_etudiant FROM `lpecom_etudiants` NATURAL JOIN `lpecom_examens` GROUP BY id_etudiant HAVING COUNT(id_etudiant) > 1 ORDER BY id_etudiant ASC;
+
 -- Monde 6-2
+-- Compter le nombre d'étudiants qui ont participés à au moins un examen.
+SELECT COUNT(id_etudiant), id_etudiant FROM `lpecom_etudiants` NATURAL JOIN `lpecom_examens` GROUP BY id_etudiant HAVING COUNT(id_etudiant) > 1 ORDER BY id_etudiant ASC;
 
 -- Monde 6-3
+-- Calculer la moyenne de l'examen portant l'id 45.
+SELECT note, matiere, id_examen FROM `lpecom_examens` WHERE id_examen = 45;
+SELECT AVG(note) FROM `lpecom_examens` WHERE id_examen = 45;
 
 -- Monde 6-4
+-- Afficher la meilleure note de l'examen portant l'id 87.
+SELECT * FROM `lpecom_etudiants` NATURAL JOIN `lpecom_examens`;
+SELECT note FROM `lpecom_etudiants` NATURAL JOIN `lpecom_examens` WHERE id_examen = 87 ORDER BY note DESC LIMIT 1;
 
 -- Monde 6-5
+-- Afficher l'id des étudiants qui ont eu plus de 11 à l'examen 45 ou plus de 12 à l'examen 87.
+SELECT * FROM `lpecom_etudiants` NATURAL JOIN `lpecom_examens`;
+SELECT * FROM `lpecom_etudiants` NATURAL JOIN `lpecom_examens` WHERE (id_examen = 45 && note > 11) || (id_examen = 87 && note > 12);
+SELECT DISTINCT id_etudiant FROM `lpecom_etudiants` NATURAL JOIN `lpecom_examens` WHERE (id_examen = 45 && note > 11) || (id_examen = 87 && note > 12);
 
 -- Monde 6-6
+-- Afficher tous les enregistrement de la table lpecom_examens avec en plus, si c'est possible, le prénom et le nom de l'étudiant.
+SELECT * FROM `lpecom_examens` NATURAL JOIN `lpecom_etudiants`;
 
 -- Monde 6-7
+-- Afficher les enregistrements de la table lpecom_examens avec le prénom et le nom de l'étudiant, uniquement quand les étudiants sont présents dans la table lpecom_etudiants.
+SELECT DISTINCT nom, prenom FROM `lpecom_etudiants` JOIN `lpecom_examens`;
 
 -- Monde 6-8
+-- Afficher uniquement le nom et le prénom de l'étudiant avec l'id 30 avec la moyenne de ses deux examens dans une colonne moyenne.
+SELECT AVG(note) AS moyenne, nom, prenom FROM `lpecom_etudiants` NATURAL JOIN `lpecom_examens` WHERE id_etudiant = 30;
 
 -- Monde 6-9
+-- Afficher les 3 meilleures examens, du meilleur au moins bon, avec le prénom et le nom de l'étudiant associé.
+SELECT DISTINCT * FROM `lpecom_etudiants` NATURAL JOIN `lpecom_examens`;
+SELECT nom, prenom, matiere, note FROM `lpecom_etudiants` NATURAL JOIN `lpecom_examens` ORDER BY note DESC LIMIT 3;
+
+-- Monde 7
+
+-- Monde 7-1
+
+-- Monde 7-2
+
+-- Monde 7-3
+
+-- Monde 7-4
+
+-- Monde 7-5
+
+-- Monde 7-6
+
+-- Monde 7-7
+
+-- Monde 7-8
+
+-- Monde 7-9
+
+-- Monde 7-10
+
+-- Monde 7-11
+
+-- Monde 7-12
+
+-- Monde 7-13
+
+-- Monde 7-14
+
+-- Monde 7-15
+
+-- Monde 7-16
+
+-- Monde 7-17
+
+-- Monde 7-18
+
+-- Monde 7-19
