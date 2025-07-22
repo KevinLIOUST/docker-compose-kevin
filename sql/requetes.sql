6910b61171f25991f457920e5ebe3e9218c07ee7
KevinLIOUST <kevin.lioust@outlook.fr>
2025-07-22 17:19:11 +0200

    Level 7 et bonus terminés  ! :)

--- sql/requetes.sql
+++ sql/requetes.sql
@@ -242,7 +242,7 @@ SELECT * FROM games;
 SELECT DISTINCT g_name FROM games ORDER BY g_name ASC;
 
 -- Monde 7-3
--- Affichez le nom, le mode de jeu, la date de sortie et le pegi de tous les jeux triés par ordre alphabétiques.
+-- Affichez le nom, le mode de jeu, la date de sortie et le pegi de tous les jeux triés par ordre alphabétique.
 SELECT g_name, g_mode, g_published_at, g_pegi FROM games ORDER BY g_name ASC;
 SELECT DISTINCT g_name, g_mode, g_published_at, g_pegi FROM games ORDER BY g_name ASC;
 
@@ -293,13 +293,232 @@ SELECT g_name, s_name, s_nationality FROM games NATURAL JOIN studios;
 SELECT g_name, g_mode, g_pegi, p_name FROM games NATURAL JOIN platforms ORDER BY g_pegi ASC;
 
 -- Monde 7-14
+-- Affichez le nom de jeu et les plateformes de tous les jeux triés par ordre alphabétique
+SELECT g_name, p_name FROM games NATURAL JOIN platforms ORDER BY g_name ASC;
 
 -- Monde 7-15
+-- Calculez le nombre total de jeux.
+SELECT COUNT(*) FROM games;
 
 -- Monde 7-16
+-- Affichez le nombre de jeux uniques.
+SELECT DISTINCT COUNT(*) FROM games;
 
 -- Monde 7-17
+-- Calculez le nombre de jeux par studio.
+SELECT s_name, COUNT(s_id) AS nb_jeux_par_studio FROM games NATURAL JOIN studios GROUP BY s_name;
 
 -- Monde 7-18
+-- Calculez le nombre de jeux par studio et par plateforme.
+SELECT s_name, p_name, COUNT(g_id) FROM games NATURAL JOIN studios NATURAL JOIN platforms NATURAL JOIN games_platforms GROUP BY s_name, p_name;
 
 -- Monde 7-19
+-- Affichez les jeux disponibles sur au moins 4 plateformes
+-- SELECT g_name, p_name, COUNT(g_name) AS nb_jeux_plateformes FROM games NATURAL JOIN platforms NATURAL JOIN games_platforms GROUP BY g_name, p_name HAVING nb_jeux_plateformes >= 4;
+SELECT g_name, COUNT(g_id) AS nb FROM games NATURAL JOIN games_platforms GROUP BY g_name HAVING nb >= 4;
+
+-- Monde 7-20
+-- Mettre le mode de jeu de Cyberpunk 2077 en solo
+UPDATE games SET g_mode = 'Solo' WHERE g_mode = "Solo / Multijoueur" AND g_name = "Cyberpunk 2077";
+
+-- Monde 7-21
+-- CREATE : “insert into” :
+-- Dans la base de données ` videogames `, enregistrer 2 jeux que vous souhaitez :
+INSERT INTO studios (s_name, s_nationality) VALUES('Gust', 'Japon');
+
+INSERT INTO games (g_mode, g_name, g_pegi, g_published_at, s_id) VALUES('Solo', 'Edens Zero', '16', '2025-07-15', "22");
+INSERT INTO games (g_mode, g_name, g_pegi, g_published_at, s_id) VALUES('Solo', 'FairyTail', '12', '2020-07-31', "22");
+
+-- id Plateforme PC : 4
+-- id Plateforme PlayStation 5 : 8
+-- id Plateforme STEAM : 10
+-- id Plateforme XBOX Series X/S : 15
+-- id Plateforme Nintendo Switch : 3
+-- id Plateforme PlayStation 4 : 7
+
+-- id Genre RPG : 5
+
+INSERT INTO games_platforms (p_id, g_id) VALUES(4, 50);
+INSERT INTO games_platforms (p_id, g_id) VALUES(8, 50);
+INSERT INTO games_platforms (p_id, g_id) VALUES(10, 50);
+INSERT INTO games_platforms (p_id, g_id) VALUES(15, 50);
+
+INSERT INTO games_platforms (p_id, g_id) VALUES(4, 51);
+INSERT INTO games_platforms (p_id, g_id) VALUES(10, 51);
+INSERT INTO games_platforms (p_id, g_id) VALUES(3, 51);
+INSERT INTO games_platforms (p_id, g_id) VALUES(7, 51);
+
+INSERT INTO games_genres (genre_id, g_id) VALUES(5, 50);
+INSERT INTO games_genres (genre_id, g_id) VALUES(5, 51);
+
+-- Monde 7-22
+-- READ : "Select" :
+-- A l’aide d’une requête, afficher les infos suivantes des “ 2 nouveaux jeux enregistrés ” :
+-- Utilisation d’ALIAS obligatoire pour avoir le même nom des champs ci-dessous :
+-- id
+-- nom
+-- genre
+-- mode de jeu
+-- pegi
+-- nom du studio
+-- plateforme(s)
+-- Pensez à utiliser la fonction `GROUP_CONCAT` si vous deviez regrouper des choses, ex. : "PC, Playstation 4, Xbox ..."
+-- SELECT
+-- games.g_id AS 'id',
+-- games.g_name AS 'nom',
+-- genres.genre_name AS 'genre',
+-- games.g_mode AS 'mode de jeu',
+-- games.g_pegi AS 'pegi',
+-- studios.s_name AS 'nom du studio',
+-- GROUP_CONCAT(platforms.p_name SEPARATOR ', ')
+-- FROM games
+-- NATURAL JOIN genres
+-- JOIN games_genres ON games_genres.genre_id = genres.genre_id
+-- NATURAL JOIN platforms
+-- NATURAL JOIN studios
+-- JOIN games_platforms ON platforms.p_id = games_platforms.p_id
+-- WHERE games.g_id = 50;
+SELECT
+g_id AS 'id',
+g_name AS 'nom',
+genre_name AS 'genre',
+g_mode AS 'mode de jeu',
+g_pegi AS 'pegi',
+s_name AS 'nom du studio',
+GROUP_CONCAT(p_name SEPARATOR ', ') AS 'platefomes'
+FROM games 
+NATURAL JOIN games_genres
+NATURAL JOIN games_platforms
+NATURAL JOIN platforms
+NATURAL JOIN genres
+NATURAL JOIN studios
+WHERE games.g_id = 50
+GROUP BY g_id, genre_name;
+
+SELECT
+g_id AS 'id',
+g_name AS 'nom',
+genre_name AS 'genre',
+g_mode AS 'mode de jeu',
+g_pegi AS 'pegi',
+s_name AS 'nom du studio',
+GROUP_CONCAT(p_name SEPARATOR ', ') AS 'platefomes'
+FROM games 
+NATURAL JOIN games_genres
+NATURAL JOIN games_platforms
+NATURAL JOIN platforms
+NATURAL JOIN genres
+NATURAL JOIN studios
+WHERE games.g_id = 51
+GROUP BY g_id, genre_name;
+
+-- Monde 7-23
+-- UPDATE "mise à jour -> update"
+-- Changer le genre du jeu : “ Naruto Shippuden Ultimate Ninja Storm 2 ” avec l’ID 39 , car il s’agit d’un jeu de “ Combat ”
+
+-- id genre combat : 13
+-- id jeu Naruto Shippuden Ultimate Ninja Storm 2 : 39
+
+UPDATE games_genres SET genre_id = 13 WHERE g_id = 39 && genre_id = 14;
+
+-- Changer le nom du jeu “ Counter-Strike : Global Offensive ” par “ Counter-Strike GO ”
+UPDATE games SET g_name = "Counter-Strike GO" WHERE g_name = "Counter-Strike : Global Offensive";
+
+-- Monde 7-24
+-- DELETE "delete -> Supprimmer"
+-- Supprimer le jeu “ Naruto Shippuden Ultimate Ninja Storm 2 ” dont l’ID est 30.
+
+-- id jeu Naruto Shippuden Ultimate Ninja Storm 2 : 30
+-- DELETE FROM `table` WHERE condition
+
+DELETE FROM games_platforms WHERE g_id = 30;
+DELETE FROM games_genres WHERE g_id = 30;
+DELETE FROM games WHERE g_id = 30;
+
+-- Supprimer le jeu “ Red Dead Redemption ” dont l’ID est 40.
+
+DELETE FROM games_platforms WHERE g_id = 40;
+DELETE FROM games_genres WHERE g_id = 40;
+DELETE FROM games WHERE g_id = 40;
+
+-- BONUS !!!! :) :) :) :)
+-- Trouver le meurtrier de SQL City
+
+SELECT * FROM crime_scene_report
+WHERE city = "SQL City"
+AND type = "murder";
+
+-- REDACTED REDACTED REDACTED
+-- Someone killed the guard! He took an arrow to the knee!
+-- Security footage shows that there were 2 witnesses. 
+-- The first witness lives at the last house on "Northwestern Dr".
+-- The second witness, named Annabel, lives somewhere on "Franklin Ave".
+
+SELECT * FROM person
+WHERE id = 16371;
+
+-- Temoin 1 :
+-- Annabel Miller :
+-- id = 16371
+-- address_street_name = Franklin Ave
+-- address_number = 103
+-- id permis de conduire : 490173
+
+SELECT * FROM interview
+WHERE person_id = 16371;
+
+-- Annabel Miller :
+-- I saw the murder happen, and I recognized the killer from my gym when I was working out last week on January the 9th 2018.
+
+SELECT * FROM drivers_license WHERE id = 490173;
+
+-- Annabel Miller :
+-- Age : 35 ans
+-- Taille : 1m65
+-- Couleur de yeux : verts
+-- Couleur de cheveux : marron
+-- Genre : fille
+-- Plaque d'immatriculation : 23AM98
+-- Marque voiture : TOYOTA
+-- Model de voiture : Yaris
+
+SELECT * FROM get_fit_now_member
+WHERE person_id = 16371;
+
+-- Annabel Miller :
+-- A commencée à travailler le 08 / 02 / 2016
+-- Statut : Gold
+-- id membre : 90081
+
+SELECT * FROM get_fit_now_check_in
+WHERE check_in_date = 20180109
+AND membership_id = 90081;
+
+-- Le 9 Janvier 2018, Annabel Miller était à la salle de Gym entre 16:00 et 17:00. D'après son interview, elle aurait reconnu le merutrier à ce moment là.
+
+-- Hyothèses :
+
+-- Suspect numero 1 :
+-- nom : Jeremy Bowers
+--  person_id = 67318
+-- membership_id : 48Z55
+-- check_in_date : 20180209
+-- check_in_time : 15:30
+-- check_out_time : 17:00
+
+-- Suspect numéro 2 :
+-- nom : Joe Germuska
+-- membership_id : 48Z7A
+-- check_in_date : 20180209
+-- check_in_time : 16:00
+-- check_out_time : 17:30
+
+-- Jeremy Bowers c'est le tueur à gage !!!! :) :) :) :)
+
+-- Son témoignage :
+
+-- I was hired by a woman with a lot of money.
+-- I don't know her name but I know she's around 5'5" (65") or 5'7" (67").
+-- She has red hair and she drives a Tesla Model S. I know that she attended the SQL Symphony Concert 3 times in December 2017.
+
+-- La commanditaire, c'est : Miranda Priestly !!!! :) :) :) :)
