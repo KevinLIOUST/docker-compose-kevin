5bfed3f6858e00d31b96172ea3f25a9f4b6d42c9
KevinLIOUST <kevin.lioust@outlook.fr>
2025-08-01 13:52:08 +0200

    Monde 4-3 avancement très bien  ! :)

--- src/tableaux/monde4-3/assets/products.php
+++ src/tableaux/monde4-3/assets/products.php
@@ -3,47 +3,115 @@ $products = [
     [
         'categorie' => 'Audio',
         'nom' => 'Casque audio',
-        'prix' => 89.99,
+        'prix' => 90,
         'description' =>
             'Casque audio confortable au son immersif, parfait pour la musique, le gaming ou les appels.',
-        'image' => 'assets/img/casque_audio.png',
+        'image' => 'assets/img/casque_audio-2.png',
     ],
     [
         'categorie' => 'Informatique',
         'nom' => 'Télé Geek RétroPixel 80s',
-        'prix' => 69.99,
+        'prix' => 70,
         'description' =>
             'Télévision rétro avec écran CRT et affichage pixelisé Space Invaders : la touche nostalgie ultime pour tout geek qui se respecte !',
-        'image' => 'assets/img/Télé_Geek_RétroPixel_80s.png',
+        'image' => 'assets/img/Télé_Geek_RétroPixel_80s-2.png',
     ],
     [
         'categorie' => 'Téléphonie',
         'nom' => 'Téléphone Geek RetroGame One',
-        'prix' => 49.9,
+        'prix' => 50,
         'description' =>
             'Un téléphone old-school avec jeu 8-bit intégré, antenne vintage et look pixelisé pour les vrais nostalgeeks !',
-        'image' => 'assets/img/Télé_Geek_RétroPixel_80s.png',
+        'image' => 'assets/img/telephone_geek-2.png',
     ],
     [
-        'categorie' => 'Téléphonie',
+        'categorie' => 'Informatique',
         'nom' =>
-            'Carte Graphique ROG ASTRAL GeForce RTX 5090 - PixelCore Galaxy Edition',
+            'Carte Graphique - ROG ASTRAL GeForce RTX 5090 - PixelCore Galaxy Edition',
         'prix' => 2349,
         'description' =>
             'Carte graphique ultra-puissante au design pixel-art rétro, parfaite pour les gamers geeks en quête de style et de performance !',
         'image' =>
-            'assets/img/Carte_graphique_NVIDIA_GeForce_RTX_5090_ASUS_Rog_ASTRAL.png',
+            'assets/img/Carte_graphique_NVIDIA_GeForce_RTX_5090_ASUS_Rog_ASTRAL-2.png',
     ],
+    [
+        'categorie' => 'Informatique',
+        'nom' =>
+            'AMD Ryzen 7 9800X3D - Otaku Boost Edition',
+        'prix' => 479,
+        'description' =>
+            'Le Ryzen 7 9800X3D Otaku Boost Edition allie performances de nouvelle génération et ADN geek assumé. Pensé pour les codeurs no-life, les gamers compétitifs et les streamers manga-addicts, ce CPU 8 cœurs dopé au cache 3D est le cœur battant de toute config légendaire !',
+        'image' =>
+            'assets/img/Processeur_AMD_Ryzen_7_9800X3D_Otaku_Boost_Edition-2.png',
+    ],
+    [
+        'categorie' => 'Informatique',
+        'nom' =>
+            'MSI MAG CoreLiquid C360 - Neo Otaku Edition',
+        'prix' => 110,
+        'description' =>
+            'Refroidis ton processeur avec style grâce à ce watercooling RGB digne d\'un shōnen : éclairs de chakra visuels, puissance silencieuse et esthétique cyber-otaku !',
+        'image' =>
+            'assets/img/Watercooling_MSI_MAG_Coreliquid_C360_Neo_Otaku_Edition-2.png',
+    ],
+    [
+        'categorie' => 'Informatique',
+        'nom' =>
+            'Carte Mère - GIGABYTE B650 Gaming X AX V2 - Neo Genesis Geek Edition',
+        'prix' => 170,
+        'description' =>
+            'Carte mère pensée pour les fans d\'animés et de gaming rétro, la Neo Genesis Geek Edition allie compatibilité Ryzen, connectivité DDR5/Wi-Fi 6E et style otaku. Elle offre à ta config un vrai boost de personnalité pixelisée et de puissance !',
+        'image' =>
+            'assets/img/Carte_Mère_Gigabyte_X_AX_V2_Neo_Genesis_Geek_Edition-2.png',
+    ],
+    [
+        'categorie' => 'Informatique',
+        'nom' =>
+            'Boîtier PC - MSI MPG GUNGNIR 110R - Heroic Pixel Edition',
+        'prix' => 115,
+        'description' =>
+            'Le MPG GUNGNIR 110R Heroic Pixel Edition est le sanctuaire ultime pour les configs de héros. Un design entre donjon high-tech et salle du trône RGB, prêt à accueillir ta carte légendaire et tes quêtes épiques. Parfait pour invoquer puissance et style dans tous les univers !',
+        'image' =>
+            'assets/img/MSI_MPG_Gungnir_110R_Heroic_Pixel_Edition-2.png',
+    ],
+    [
+        'categorie' => 'Informatique',
+        'nom' =>
+            'Thermaltake Toughpower GT 850W - Pixel Storm Edition',
+        'prix' => 110,
+        'description' =>
+            'L\'alimentation Pixel Storm Edition délivre ses 850W comme un sort épique : stabilité, silence, et style. Idéale pour les setups qui brillent aussi fort que le cœur d\'un héros shōnen en plein power-up !',
+        'image' =>
+            'assets/img/Thermaltake_Toughpower_GT_850W_Pixel_Storm_Edition-2.png',
+    ],
+    [
+        'categorie' => 'Informatique',
+        'nom' =>
+            'Creative Pebbles Pro 2.0 - KameSound Edition',
+        'prix' => 59,
+        'description' =>
+            'Les enceintes creative Pebbles Pro 2.0 - KameSound Edition combinent un design sphérique galactique et un son percutant digne d\'une arène finale de boss. Compactes mais puissantes, elles transforment chaque morceau en moment épique d\'anime ou de JRPG.',
+        'image' =>
+            'assets/img/Creative_Peebles_Pro_2-0_KameSound_Edition-2.png',
+    ],
+    [
+        'categorie' => 'Informatique',
+        'nom' =>
+            'Cocopa USB 3.0 CD/DVD - RetroGalaxy Edition',
+        'prix' => 20,
+        'description' =>
+            'Le Cocopa RetroGalaxy Edition te permet de lire, graver et explorer tes disques comme à l\'époque héroïque des JRPG sur PC ! Embarque ton vieux stock d\'OST, tes collectors DVD anime et redonne vie à ta nostalgie en USB 3.0 !',
+        'image' =>
+            'assets/img/Lecteur_CD-DVD_RetroGalaxy_Edition-2.png',
+    ],
+    [
+        'categorie' => 'Informatique',
+        'nom' =>
+            'MSI Vigor GK30 - Hikari RGB Otaku Edition',
+        'prix' => 68,
+        'description' =>
+            'Le GK30 Hikari RGB Otaku Edition allie performance MSI et esthétisme pixel-anime. Parfait pour coder, jouer, et écrire ton prochain fanfic de l\'univers multivers en RGB, ce clavier transforme chaque touche en une incantation geek !',
+        'image' =>
+            'assets/img/Clavier_MSI-2.png',
+    ]
 ]; ?>
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Monde 4-3 - Les tableaux !!!!</title>
-</head>
-<body>
-
-</body>
-</html>
--- src/tableaux/monde4-3/index.php
+++ src/tableaux/monde4-3/index.php
@@ -1,6 +1,6 @@
 <?php
 require_once 'assets/products.php';
-var_dump($products);
+// var_dump($products);
 ?>
 
 <!DOCTYPE html>
@@ -9,8 +9,52 @@ var_dump($products);
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Monde 4-3 - Les tableaux !!!!</title>
+
+    <!-- Lien vers Bootstrap -->
+    <link rel="stylesheet" href="../../node_modules/bootstrap/dist/css/bootstrap.min.css" />
+
+    <!-- Lien vers Materialize -->
+    <link rel="stylesheet" href="../../node_modules/materialize-css/dist/css/materialize.min.css" />
+
+    <!-- Lien vers les icônes Bootstrap -->
+    <link rel="stylesheet" href="../../node_modules/bootstrap-icons/font/bootstrap-icons.min.css">
+
+    <!-- Lien pour utiliser JavaScript pour les actions de l'utilisateur pour Bootstrap -->
+    <script src="../../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO"
+        crossorigin="anonymous"></script>
+
+        <link rel="stylesheet" href="assets/css/style.css">
 </head>
 <body>
-
+    <div class="text-center">
+        <h1>Magasin d'informatique</h1>
+        <h2>Vous trouverez des composants spécial Geek pour votre PC !!!!</h2>
+    </div>
+    <div class="text-center">
+        <div class="container">
+            <?php $i = 0;?>
+            <?php foreach ($products as $key => $value) { ?>
+                <div class="card">
+                    <div>
+                        <p><?= $products[$i]['categorie'] ?></p>
+                    </div>
+                    <div class="card-image waves-effect waves-block waves-light">
+                        <img class="activator" src="<?= $products[$i]['image'] ?>">
+                    </div>
+                    <div class="card-content">
+                        <span class="activator grey-text text-darken-4"><?= $products[$i]['nom'] ?><i class="material bi bi-three-dots-vertical"></i></span>
+                        <p>Prix : <?= $money = $products[$i]['prix']; ?></p>
+                        <p><a href="#">Faux lien</a></p>
+                    </div>
+                    <div class="card-reveal">
+                        <span class="grey-text text-darken-4"><?= $products[$i]['nom'] ?><i class="material-icons right">close</i></span>
+                        <p><?= $products[$i]['description'] ?></p>
+                    </div>
+                </div>
+                <?php $i++ ?>
+            <?php } ?>
+        </div>
+    </div>
 </body>
 </html>
